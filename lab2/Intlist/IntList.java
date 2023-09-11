3f861bcc8f322fde634bd57fe7865a4a75b8b0c9
anruizhe <1181678485@qq.com>
2023-09-11 18:40:05 +0800

    lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,8 +81,14 @@ public class IntList {
      */
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        if(A == null || B == null){
-            return  A;
+        if(A == null){
+            return B;
+        }
+        else if (B == null){
+            return A;
+        }
+        else if (A == null || B == null){
+            return null;
         }
         else{
             IntList temp = A;
