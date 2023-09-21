3b22da6af5d301932428b580de9e39d7c20e29ae
anruizhe <1181678485@qq.com>
2023-09-21 21:55:49 +0800

    proj1b

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -24,5 +24,13 @@ public class TestOffByOne {
 
         assertFalse(offByOne.equalChars('a', 'A'));
         System.out.println("case5 passed");
+
+        assertTrue(offByOne.equalChars('a', 'b'));
+        assertTrue(offByOne.equalChars('r', 'q'));
+        assertTrue(offByOne.equalChars('&', '%'));
+        assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('a', 'z'));
+        assertFalse(offByOne.equalChars('a', 'e'));
+        assertFalse(offByOne.equalChars('a', 'A'));
     }
 }
