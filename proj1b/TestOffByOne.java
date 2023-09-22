8a30bb53c9e8ac26242d5b97a8979625d00f0157
anruizhe <1181678485@qq.com>
2023-09-22 16:57:32 +0800

    proj1b

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -10,41 +10,34 @@ public class TestOffByOne {
     // Your tests go here.
     @Test
     public void testequalChars() {
-        /*assertTrue(offByOne.equalChars('a', 'b'));
-        System.out.println("case1 passed");
-
-        assertFalse(offByOne.equalChars('a', 'c'));
-        System.out.println("case2 passed");
-
-        assertTrue(offByOne.equalChars('&', '%'));
-        System.out.println("case3 passed");
-
-        assertFalse(offByOne.equalChars(' ', '0'));
-        System.out.println("case4 passed");
-
-        assertFalse(offByOne.equalChars('a', 'A'));
-        System.out.println("case5 passed");
-
-        assertFalse(offByOne.equalChars('a', 'a'));
-        System.out.println("case6 passed");
-
-        assertFalse(offByOne.equalChars('a', 'z'));
-        System.out.println("case7 passed");
-
-        assertTrue(offByOne.equalChars('r', 'q'));
-        System.out.println("case8 passed");
-
-        assertFalse(offByOne.equalChars('a', 'z'));
-        System.out.println("case9 passed");
-
-        assertFalse(offByOne.equalChars('a', 'e'));
-        System.out.println("case10 passed");*/
         assertTrue(offByOne.equalChars('a', 'b'));
+
         assertTrue(offByOne.equalChars('r', 'q'));
+
         assertTrue(offByOne.equalChars('&', '%'));
-        assertFalse(offByOne.equalChars('a', 'a'));
-        assertFalse(offByOne.equalChars('a', 'z'));
+
+        assertTrue(offByOne.equalChars('A', 'B'));
+
+        assertTrue(offByOne.equalChars('B', 'A'));
+
+        assertFalse(offByOne.equalChars('A', 'C'));
+
+        assertFalse(offByOne.equalChars('A', 'A'));
+
         assertFalse(offByOne.equalChars('a', 'e'));
+
+        assertFalse(offByOne.equalChars('a', 'a'));
+
         assertFalse(offByOne.equalChars('a', 'A'));
+
+        assertFalse(offByOne.equalChars('a', 'B'));
+
+        assertFalse(offByOne.equalChars('A', 'b'));
+
+        assertFalse(offByOne.equalChars('a', '%'));
+
+        assertFalse(offByOne.equalChars('&', '$'));
+
+        System.out.println("all cases passed");
     }
 }
