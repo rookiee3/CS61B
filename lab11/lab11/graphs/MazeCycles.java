ca42ed284649d31aa3ec9b79ebf6c4e438c21717
anruizhe <1181678485@qq.com>
2023-11-14 17:05:59 +0800

    lab11

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,6 @@
 package lab11.graphs;
 
+import java.util.Random;
 /**
  *  @author Josh Hug
  */
@@ -9,6 +10,8 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int[] pathTo;
+    private boolean circleFound = false;
 
     public MazeCycles(Maze m) {
         super(m);
@@ -16,9 +19,41 @@ public class MazeCycles extends MazeExplorer {
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        pathTo = new int[maze.V()];
+        // int s = 0;
+        Random rand = new Random();
+        int startX = rand.nextInt(maze.N());
+        int startY = rand.nextInt(maze.N());
+        int s = maze.xyTo1D(startX, startY);
+        pathTo[s] = s;
+        dfs(s);
+        announce();
     }
 
     // Helper methods go here
+    private void dfs(int v) {
+        marked[v] = true;
+        for (int w : maze.adj(v)) {
+            if (circleFound) {
+                return;
+            }
+            if (!marked[w]) {
+                pathTo[w] = v;
+                dfs(w);
+            } else if (w != pathTo[v]) {
+                //find cycle
+                pathTo[w] = v;
+                int cur = v;
+                edgeTo[cur] = pathTo[cur];
+                while (cur != w) {
+                    cur = pathTo[cur];
+                    edgeTo[cur] = pathTo[cur];
+                }
+                circleFound = true;
+                return;
+            }
+        }
+    }
+
 }
 
